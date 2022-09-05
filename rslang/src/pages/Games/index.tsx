db952382454f94335422d83cf4023d8054583b58
Igor Shah <zombee1992@gmail.com>
2022-09-05 22:47:13 +0300

    BasicCard: adjust to several photos

--- rslang/src/pages/Games/index.tsx
+++ rslang/src/pages/Games/index.tsx
@@ -5,14 +5,19 @@ import React from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { useAudiocallActionsCreators, useSprintActionsCreators } from "../../hooks/useActions"
 import { BasicCard } from "./components/BasicCard"
+import image from "../../../assets/img/audiobook.jpg"
+import imageOne from "../../assets/img/audiobook.jpg"
+import imageTwo from "../../assets/img/audiobook.jpg"
 
 const game1 = {
   name: "audiocall",
   article: "some audiocall",
+  image:imageOne,
 }
 const game2 = {
   name: "sprint",
   article: "some sptint",
+  image:imageTwo,
 }
 function Games() {
   const { sprintSetLevel, sprintSetStart } = useSprintActionsCreators()
