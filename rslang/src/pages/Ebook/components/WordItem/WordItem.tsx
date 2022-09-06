203d196d48ac2cd914edc38f8d69fa818b2ddd8a
Igor Shah <zombee1992@gmail.com>
2022-09-06 14:49:51 +0300

    #15 add wrapeers and containers

--- rslang/src/pages/Ebook/components/WordItem/WordItem.tsx
+++ rslang/src/pages/Ebook/components/WordItem/WordItem.tsx
@@ -26,111 +26,120 @@ export default function WordItem({ dataWord }: { dataWord: IUserWordsWithCurrent
   return (
     <>
       <div className={styles.wordcontainer}>
-        <img className={styles.wordImage} src={"https://rslang-rss.herokuapp.com/" + dataWord.image}></img>
-        <div className={styles.wordInfo}>
-          <div className={styles.wordItem}>
-            <p className={styles.word}>{dataWord.word}</p>
-            <p className={styles.wordTranscription}>{dataWord.transcription}</p>
-            <p className={styles.wordTranslate}>{dataWord.wordTranslate}</p>
+        <div className={styles.wordCard}>
+          <img className={styles.wordImage} src={"https://rslang-rss.herokuapp.com/" + dataWord.image}></img>
+          <div className={styles.wordInfo}>
+            <div className={styles.wordItem}>
+              <p className={styles.word}>{dataWord.word}</p>
+              <p className={styles.wordTranscription}>{dataWord.transcription}</p>
+              <p className={styles.wordTranslate}>{dataWord.wordTranslate}</p>
+            </div>
+            <div className={styles.wordSubinfo}>
+              <div className={styles.wordsExample}>
+                <p className={styles.wordText} dangerouslySetInnerHTML={{ __html: dataWord.textExample }}></p>
+                <p
+                  className={styles.wordTextTranslate}
+                  dangerouslySetInnerHTML={{ __html: dataWord.textExampleTranslate }}
+                ></p>
+              </div>
+              <div className={styles.wordsExample}>
+                <p className={styles.wordText} dangerouslySetInnerHTML={{ __html: dataWord.textMeaning }}></p>
+                <p
+                  className={styles.wordTextTranslate}
+                  dangerouslySetInnerHTML={{ __html: dataWord.textMeaningTranslate }}
+                ></p>
+              </div>
+            </div>
           </div>
-          <div className={styles.wordSubinfo}>
-            <div className={styles.wordsExample}>
-              <p className={styles.wordText} dangerouslySetInnerHTML={{ __html: dataWord.textExample }}></p>
-              <p
-                className={styles.wordTextTranslate}
-                dangerouslySetInnerHTML={{ __html: dataWord.textExampleTranslate }}
-              ></p>
-            </div>
-            <div className={styles.wordsExample}>
-              <p className={styles.wordText} dangerouslySetInnerHTML={{ __html: dataWord.textMeaning }}></p>
-              <p
-                className={styles.wordTextTranslate}
-                dangerouslySetInnerHTML={{ __html: dataWord.textMeaningTranslate }}
-              ></p>
-            </div>
+
+          <div className={styles.btnContainer}>
+            <img draggable="false" src={playBtn} className={styles.playBtn} onClick={playButton}></img>
           </div>
         </div>
 
-        <div className={styles.btnContainer}>
-          <img draggable="false" src={playBtn} className={styles.playBtn} onClick={playButton}></img>
+        <div className={styles.optionalWrapper}>
+          <div
+            style={
+              {
+                // display: "flex",
+                // marginTop: -20,
+                // gap: 10,
+                // justifyContent: "center",
+                // flexWrap: "wrap",
+                // listStyle: "none",
+              }
+            }
+          >
+            {localStorage.getItem("user") ? (
+              <>
+                <button
+                  style={{ width: 200, backgroundColor: dataWord?.difficulty === "hard" ? "green" : "" }}
+                  onClick={() => setDificultyUserWord(dataWord.id, userWords)}
+                >
+                  <p>Hard</p>
+                </button>
+                <button
+                  // style={{ width: 200, backgroundColor: dataWord?.optional?.isLearned === true ? "green" : "" }}
+                  onClick={() => setLearndUserWord(dataWord.id, userWords)}
+                >
+                  <p>Learnd</p>
+                </button>
+              </>
+            ) : (
+              <>
+                <Link to="/signin">
+                  <button style={{ width: 200, backgroundColor: dataWord.difficulty === "hard" ? "green" : "" }}>
+                    <p>Hard</p>
+                  </button>
+                </Link>
+                <Link to="/signin">
+                  <button
+                    style={{ width: 200, backgroundColor: dataWord?.optional?.isLearned === true ? "green" : "" }}
+                  >
+                    <p>Learnd</p>
+                  </button>
+                </Link>
+              </>
+            )}
+          </div>
+          {dataWord.optional ? (
+            <ul
+              className={styles.optionalContainer}
+              style={{
+                display: "flex",
+                marginTop: -20,
+                justifyContent: "space-between",
+                flexWrap: "wrap",
+                listStyle: "none",
+              }}
+            >
+              <li>difficulty: {dataWord.difficulty}</li>
+              <li>Is learnd: {dataWord.optional.isLearned ? "yes" : "no"}</li>
+              <li>
+                Progress bar: {dataWord.optional.progressBar} / {dataWord.optional.progressBarSize}
+              </li>
+              <li>is New: {dataWord.optional.isNew ? "yes" : "no"}</li>
+              <li>meetingCounter: {dataWord.optional.meetingCounter}</li>
+            </ul>
+          ) : (
+            <ul
+              style={{
+                display: "flex",
+                marginTop: -20,
+                justifyContent: "space-between",
+                flexWrap: "wrap",
+                listStyle: "none",
+              }}
+            >
+              <li>difficulty: null</li>
+              <li>Is learnd: null</li>
+              <li>Progress bar: null</li>
+              <li>is New: null</li>
+              <li>meetingCounter: null</li>
+            </ul>
+          )}
         </div>
       </div>
-
-      <div
-        style={{
-          display: "flex",
-          marginTop: -20,
-          gap: 10,
-          justifyContent: "center",
-          flexWrap: "wrap",
-          listStyle: "none",
-        }}
-      >
-        {localStorage.getItem("user") ? (
-          <>
-            <button
-              style={{ width: 200, backgroundColor: dataWord?.difficulty === "hard" ? "green" : "" }}
-              onClick={() => setDificultyUserWord(dataWord.id, userWords)}
-            >
-              <p>Hard</p>
-            </button>
-            <button
-              style={{ width: 200, backgroundColor: dataWord?.optional?.isLearned === true ? "green" : "" }}
-              onClick={() => setLearndUserWord(dataWord.id, userWords)}
-            >
-              <p>Learnd</p>
-            </button>
-          </>
-        ) : (
-          <>
-            <Link to="/signin">
-              <button style={{ width: 200, backgroundColor: dataWord.difficulty === "hard" ? "green" : "" }}>
-                <p>Hard</p>
-              </button>
-            </Link>
-            <Link to="/signin">
-              <button style={{ width: 200, backgroundColor: dataWord?.optional?.isLearned === true ? "green" : "" }}>
-                <p>Learnd</p>
-              </button>
-            </Link>
-          </>
-        )}
-      </div>
-      {dataWord.optional ? (
-        <ul
-          style={{
-            display: "flex",
-            marginTop: -20,
-            justifyContent: "space-between",
-            flexWrap: "wrap",
-            listStyle: "none",
-          }}
-        >
-          <li>difficulty: {dataWord.difficulty}</li>
-          <li>Is learnd: {dataWord.optional.isLearned ? "yes" : "no"}</li>
-          <li>
-            Progress bar: {dataWord.optional.progressBar} / {dataWord.optional.progressBarSize}
-          </li>
-          <li>is New: {dataWord.optional.isNew ? "yes" : "no"}</li>
-          <li>meetingCounter: {dataWord.optional.meetingCounter}</li>
-        </ul>
-      ) : (
-        <ul
-          style={{
-            display: "flex",
-            marginTop: -20,
-            justifyContent: "space-between",
-            flexWrap: "wrap",
-            listStyle: "none",
-          }}
-        >
-          <li>difficulty: null</li>
-          <li>Is learnd: null</li>
-          <li>Progress bar: null</li>
-          <li>is New: null</li>
-          <li>meetingCounter: null</li>
-        </ul>
-      )}
     </>
   )
 }
