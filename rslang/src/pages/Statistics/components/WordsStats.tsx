df41599b2ef69d004a4a1d3035383501f9203b91
Igor Shah <zombee1992@gmail.com>
2022-09-07 00:14:36 +0300

    #23 props hell

--- rslang/src/pages/Statistics/components/WordsStats.tsx
+++ rslang/src/pages/Statistics/components/WordsStats.tsx
@@ -2,8 +2,12 @@ import React from "react"
 import styles from '../Stats.module.scss'
 
 export function WordsStats() {
+  const obj = {}
   return (
     <div className={styles.wordsStats}>
+      <p>accurate</p>
+      <p>newWords</p>
+      <p>Some</p>
     </div>
   )
 }
--- rslang/src/pages/Statistics/components/charts/BarChart.tsx
+++ rslang/src/pages/Statistics/components/charts/BarChart.tsx
@@ -1,59 +1,67 @@
-import React from "react"
-import styles from '../Stats.module.scss'
+import React, { useCallback, useEffect, useState } from "react"
+import styles from "../Stats.module.scss"
 
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js"
 
-import { Bar } from 'react-chartjs-2'
+import { Bar } from "react-chartjs-2"
+import { FullStatsData } from "../../../../models/StatsModels"
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+type props = {
+  data: FullStatsData | undefined
+}
+type graphPropsData = {
+  labels: string[]
+  datasets: {
+    label: string
+    data: number[]
+    backgroundColor: string
+  }[]
+}
 
-export function BarChart() {
+export function BarChart(props: props) {
   const options = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top' as const,
+        position: "top" as const,
       },
-      // title: {
-      //   display: true,
-      //   text: 'Chart.js Bar Chart',
-      // },
     },
-  };
+  }
 
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']; //динамически формировать массив дат
+  const [labels, setLabels] = useState<Array<string>>([])
+  const [graphProps, setGraphProps] = useState<graphPropsData>()
+  function propsObj(labels:Array<string>,data:number[]):graphPropsData{
+    return {
+      labels:labels,
+      datasets: [
+        {
+          label: "Dataset 1",
+          data: [1,23],
+          backgroundColor: "rgba(255, 99, 132, 0.5)",
+        },
+      ]
+    }
+  }
+  const a = () => 123
+  useEffect(() => {
+    const data = Object.entries(props.data || 0)
+    const labels = data.map((i) => i[0]) as string[]
+    if (labels.length) setLabels([...labels])
+    console.log("", labels)
+  }, [props])
 
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: [1, 2, 3, 4, 5, 6, 7, 8],
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      // {
-      //   label: 'Dataset 2',
-      //   data: [0, 32, 3, 4, 32, 2],
-      //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      // },
-    ],
-  };
-  return (
-    <Bar options={options} data={data} />
-  )
+  useEffect(() => {
+    const data = Object.entries(props.data || 0)
+    const newWords = data.map((i) => i[1])
+    propsObj(['10'],[1])
+    // if(newWords.length) setGraphProps(() => {})
+    console.log("", labels)
+  }, [props])
+
+  // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']; //динамически формировать массив дат
+
+
+
+  return <Bar options={options} data={data} />
 }
