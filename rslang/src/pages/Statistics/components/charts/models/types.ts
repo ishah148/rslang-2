71410bbbee38b2ff4d8084d79b9837b6ca3900d2
Nikolay <nikolay.kovzik@gmail.com>
2022-09-08 14:42:01 +0300

    refactor: renaming

--- rslang/src/pages/Statistics/components/charts/models/types.ts
+++ rslang/src/pages/Statistics/components/charts/models/types.ts
@@ -9,11 +9,11 @@ export type graphPropsData = {
 }
 
 export const defaultGraphPropsData = {
-  labels:['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  labels:['9/7/2022', '9/8/2022', '9/9/2022', '9/10/2022', '9/11/2022', '9/12/2022', '9/13/2022'],
   datasets: [
     {
       label: "Dataset 1",
-      data: [1, 2, 3, 4, 5, 6, 7, 8],
+      data: [0, 0, 0, 0, 0, 0, 0, 0],
       backgroundColor: "rgba(255, 99, 132, 0.5)",
     },
   ],
